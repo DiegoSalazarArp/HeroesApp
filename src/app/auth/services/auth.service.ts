a0911c7fcb0dce3cc1766ce1046418d7cd7f074c
Diego Salazar Arp <d.salazararp@gmail.com>
2021-05-02 17:49:58 -0400

    Seguridad de rutas, login, guards

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Auth } from '../interfaces/auth.interface';
+import { map, tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private baseURL: string = environment.baseUrl;
+  private _auth: Auth | undefined;
+
+  get auth(): Auth {
+    return { ...this._auth! };
+  }
+
+  constructor(private http: HttpClient) {}
+
+  verificaAutenticacion(): Observable<boolean> {
+    if (!localStorage.getItem('token')) {
+      return of(false);
+    }
+
+    return this.http.get<Auth>(`${this.baseURL}/usuarios/1`).pipe(
+      map((auth) => {
+        this._auth = auth;
+
+        return true;
+      })
+    );
+  }
+  login() {
+    return this.http.get<Auth>(`${this.baseURL}/usuarios/1`).pipe(
+      tap((auth) => (this._auth = auth)),
+      tap((auth) => localStorage.setItem('token', auth.id))
+    );
+  }
+
+  logout() {
+    this._auth = undefined;
+  }
+}
